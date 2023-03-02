42157a9d472bb6505ea4d3a8c14cacf2d818b8ee
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2023-03-02 14:59:03 +0100

    bring new game API features to frontend, duration, intensity etc.

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,10 @@ function App() {
     port: 5555,
   });
 
+  const [intensity, setIntensity] = useState(0.75);
+  const [duration, setDuration] = useState(10);
+  const [inifinite, setInfinite] = useState(false);
+
   return (
     <div className="flex flex-col">
       <div className="p-2 bg-slate-300">
@@ -18,19 +22,61 @@ function App() {
       <div className="flex flex-col gap-2 p-2">
         <ActionButton
           label="Vignette"
-          networkSettings={networkSettings}
           path="StartVignette"
+          intensity={intensity}
+          duration={duration}
+          inifinite={inifinite}
         />
         <ActionButton
           label="Depth of Field"
-          networkSettings={networkSettings}
           path="StartDOF"
+          intensity={intensity}
+          duration={duration}
+          inifinite={inifinite}
         />
         <ActionButton
           label="Color Loss"
-          networkSettings={networkSettings}
           path="StartColorLoss"
+          intensity={intensity}
+          duration={duration}
+          inifinite={inifinite}
         />
+        <ActionButton
+          style="danger"
+          label="Stop All Effects"
+          path="StopEffects"
+          actionWithoutParams
+        />
+        <p className="font-bold">Intensity ({intensity * 100}%) </p>
+        <input
+          className="w-full bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-300  range-lg  h-3 "
+          type="range"
+          min="0"
+          max="1"
+          step="0.01"
+          value={intensity}
+          onChange={(e) => setIntensity(parseFloat(e.target.value))}
+        />
+        <p className="font-bold">Duration ({duration} seconds)</p>
+        <input
+          className="w-full bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-300  range-lg  h-3 "
+          type="range"
+          disabled={inifinite}
+          min="1"
+          max="60"
+          step="1"
+          value={duration}
+          onChange={(e) => setDuration(parseInt(e.target.value))}
+        />
+        <div className="flex flex-row gap-2">
+          <input
+            className="w-8 h-8"
+            type="checkbox"
+            checked={inifinite}
+            onChange={(e) => setInfinite(e.target.checked)}
+          />
+          <p className="font-bold">Infinite</p>
+        </div>
       </div>
     </div>
   );
--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -4,11 +4,34 @@ import { NetworkSettings } from "../types/NetworkSettings";
 type Props = {
   label: string;
   path: string;
+  inifinite?: boolean;
+  duration?: number;
+  intensity?: number;
+  style?: "normal" | "danger";
+  actionWithoutParams?: boolean;
 };
 
-export default function ActionButton({ label, path }: Props) {
+export default function ActionButton({
+  label,
+  path,
+  style = "normal",
+  inifinite = false,
+  duration = 10,
+  intensity = 0.75,
+  actionWithoutParams = false,
+}: Props) {
+  function requestUriBuilder() {
+    const uri = `/${path}?infinite=${encodeURIComponent(
+      inifinite
+    )}&duration=${encodeURIComponent(duration)}&intensity=${encodeURIComponent(
+      intensity
+    )}`;
+    console.log(uri);
+    return uri;
+  }
+
   function triggerAction() {
-    fetch(`/${path}`, {
+    fetch(actionWithoutParams ? `/${path}` : requestUriBuilder(), {
       method: "GET",
     }).then((response) => {
       if (response.ok) {
@@ -19,9 +42,18 @@ export default function ActionButton({ label, path }: Props) {
     });
   }
 
+  function selectStyle() {
+    switch (style) {
+      case "normal":
+        return "bg-blue-500 hover:bg-blue-700";
+      case "danger":
+        return "bg-red-500 hover:bg-red-700";
+    }
+  }
+
   return (
     <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      className={`${selectStyle()} text-white font-bold py-2 px-4 rounded`}
       onClick={triggerAction}
     >
       {label}
